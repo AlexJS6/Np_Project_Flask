b30a3a4e18b0cdf25ecf2bfca53d95ca54a35f99
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-15 14:38:40 +0100

    Adding first steps of sqlalchemy

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'hello'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(hours = 24)
+
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column('id', db.Integer, primary_key = True)
+    password = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, password, email):
+        self.password = password
+        self.email = email
 
 @app.route('/')
 def index():
@@ -49,4 +64,5 @@ def logout():
     return redirect(url_for('signin'))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
