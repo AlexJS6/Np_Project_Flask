5468adb3e11b4fa356ebe40b459baca5cf5fc013
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-22 21:18:53 +0100

    Adding flights query to cart route

--- app.py
+++ app.py
@@ -298,7 +298,9 @@ def process_flights():
 
 @app.route('/cart')
 def cart():
-    return render_template('cart.html')
+    flights = flights.query.filter_by(flights._id == session['id'])
+    
+    return render_template('cart.html', flights = flights)
 
 
 if __name__ == "__main__":
