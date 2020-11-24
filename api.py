e31c54d9d3a611200e630add822cc47dd62f94fc
AlexJS6 <alex.spiesberger@gmail.com>
2020-11-24 16:24:25 +0100

    Adding cars.html

--- api.py
+++ api.py
@@ -42,3 +42,11 @@ def flight_api():
         #return requests.request('GET', url, headers = headers, params = querystring).json()
         #return f'<div style="background-color: red"><p>{departure_date}</br>{departure_time}</br>{price}</br>{carrier_id}</br>{name}</br>{symbol}</br>{origin_country}</br>{origin_city}</br>{origin_airport}</br>{destination_country}</br>{destination_city}</br>{destination_airport}</br>{direct}</p></div>'
         return render_template('flight_result.html', date = departure_date, time = departure_time, price = price, carrier_id = carrier_id, name = name, symbol = symbol, origin_country = origin_country, origin_city = origin_city, origin_airport = origin_airport, destination_country = destination_country, destination_city = destination_city, destination_airport = destination_city, direct = direct)
+
+
+
+
+@api.route('/car', methods = ['POST', 'GET'])
+def flight_api():
+    if request.method == 'GET':
+        
